99cce5885889af3e306a11d7b058f57c88ee1b28
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-20 11:50:19 +0530

    [Ganesh] Add:Generics Problems file is added

---
+++ GenericsProblems/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GenericsProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics Problem Statement");
+        }
+    }
+}
