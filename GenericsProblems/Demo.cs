b5f77c00804dac4bfddd124e791f4370ffffc3b5
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-20 12:01:02 +0530

    [Ganesh] Add:Finding max of integer is added

---
+++ GenericsProblems/Demo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblems
+{
+    public class Demo
+    {
+        public void FindIntMax(int a,int b,int c)
+        {
+            if (a > b && a > c)
+                Console.WriteLine(a + " is maximum");
+            if (b > a && b > c)
+                Console.WriteLine(b + " is maximum");
+            else
+                Console.WriteLine(c + " is maximum");
+        }
+    }
+}
--- GenericsProblems/Program.cs
+++ GenericsProblems/Program.cs
@@ -5,7 +5,14 @@ namespace GenericsProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Generics Problem Statement");
+            Demo demo=new Demo();
+            Console.Write("Enter first integer value: ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter first integer value: ");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter first integer value: ");
+            int input3 = Convert.ToInt32(Console.ReadLine());
+            demo.FindIntMax(input1,input2,input3);
         }
     }
 }
